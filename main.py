2fe0e79293e9dd1c68b840d7d43a00b4c8baa34f
mralimov <mralimov89@gmail.com>
2021-04-20 22:43:35 -0400

    Timer Reset function

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
 timer = None
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
